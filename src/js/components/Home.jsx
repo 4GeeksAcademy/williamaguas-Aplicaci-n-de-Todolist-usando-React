53ee34767675517ae2ec6bf62890245cb31ad38d
William Aguas <148153196+WILLIAMAGUAS@users.noreply.github.com>
2025-10-21 03:02:04 +0000

    primer avance

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let [tarea, setTarea] = useState("")
+	let [lista, setLista] = useState([])
+
+	const escribirTarea = (event) => {
+		setTarea(event.target.value)
+	}
+	const agregar = (event) => {
+		if (event.key === "Enter") {
+			setLista([...lista, tarea])
+			setTarea("")
+		}
+	}
+const eliminarTarea = (posicion) =>{
+	setLista(lista.filter((item, index)=>index!==posicion))
+}
 	return (
 		<div className="text-center">
-            
+			<input type="text" placeholder="agrega una tarea" onChange={escribirTarea} value={tarea} onKeyDown={agregar} />
+			<ul className="list-unstyled">
+				{lista.map((item, index) => (<li key={index}>{item}<span onClick={()=>eliminarTarea(index)}> ❌ </span></li>))}
+			</ul>
+		</div >
+	)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
-
+}
 export default Home;
